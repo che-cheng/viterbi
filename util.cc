8dc8c412ecec4ebe6b1029db19b8a0ac07e3b5eb
cheng <cheng.chang@gallopwave.com>
2021-05-24 22:19:36 +0800

    First commit for viterbi algo

--- util.cc
+++ util.cc
@@ -6,150 +6,131 @@
 // you may not use this file expect in compliance with the License.
 
 #include "util.h"
+
 #include <string>
 
-float** create_array(const int row, const int col)
-{
-    float **p;
-    p = new float*[row];
-    for (int i = 0; i <= row; ++i)
-        p[i] = new float[col];
-    return p;
+float **create_array(const int row, const int col) {
+  float **p;
+  p = new float *[row];
+  for (int i = 0; i <= row; ++i) p[i] = new float[col];
+  return p;
 }
 
-float* create_array(const int col)
-{
-    float *p;
-    p = new float[col];
-    return p;
+float *create_array(const int col) {
+  float *p;
+  p = new float[col];
+  return p;
 }
 
-int** create_int_array(const int row, const int col)
-{
-    int **p;
-    p = new int*[row];
-    for (int i = 0; i <= row; ++i)
-        p[i] = new int[col];
-    return p;
+int **create_int_array(const int row, const int col) {
+  int **p;
+  p = new int *[row];
+  for (int i = 0; i <= row; ++i) p[i] = new int[col];
+  return p;
 }
 
-int* create_int_array(const int col)
-{
-    int *p;
-    p = new int[col];
-    return p;
+int *create_int_array(const int col) {
+  int *p;
+  p = new int[col];
+  return p;
 }
 
 // read file into a 2-d array
-void read_file_into_2d_array(const string file_name, const int row, const int col, float** data)
-{
-    std::ifstream file(file_name);
+void read_file_into_2d_array(const string file_name, const int row,
+                             const int col, float **data) {
+  std::ifstream file(file_name);
 
-    for (int i = 0; i < row; ++i)
-    {
-        std::string line;
-        std::getline(file, line);
-        if(!file.good())
-            break;
+  for (int i = 0; i < row; ++i) {
+    std::string line;
+    std::getline(file, line);
+    if (!file.good()) break;
 
-        std::stringstream iss(line);
+    std::stringstream iss(line);
 
-        for (int j = 0; j < col; ++j)
-        {
-            std::string val;
-            std::getline(iss, val, ',');
-            if (!iss.good())
-                break;
+    for (int j = 0; j < col; ++j) {
+      std::string val;
+      std::getline(iss, val, ',');
+      if (!iss.good()) break;
 
-            std::stringstream convertor(val);
-            convertor >> data[i][j];
-        }
+      std::stringstream convertor(val);
+      convertor >> data[i][j];
     }
+  }
 }
 
 // read file into an 1-d array(float)
-void read_file_into_1d_array(const string file_name, int col, float* data)
-{
-    std::ifstream file(file_name);
+void read_file_into_1d_array(const string file_name, int col, float *data) {
+  std::ifstream file(file_name);
 
-    std::string line;
-    std::getline(file, line);
-    if(!file.good())
-        return;
+  std::string line;
+  std::getline(file, line);
+  if (!file.good()) return;
 
-    std::stringstream iss(line);
+  std::stringstream iss(line);
 
-    for (int j = 0; j < col; ++j)
-    {
-        std::string val;
-        std::getline(iss, val, ',');
-        if (!iss.good())
-            break;
+  for (int j = 0; j < col; ++j) {
+    std::string val;
+    std::getline(iss, val, ',');
+    if (!iss.good()) break;
 
-        std::stringstream convertor(val);
-        convertor >> data[j];
-    }
+    std::stringstream convertor(val);
+    convertor >> data[j];
+  }
 }
 
 // read file into an 1-d array(int)
-void read_file_into_1d_array(const string file_name, const int col, int* data)
-{
-    std::ifstream file(file_name);
+void read_file_into_1d_array(const string file_name, const int col, int *data) {
+  std::ifstream file(file_name);
 
-    std::string line;
-    std::getline(file, line);
-    if(!file.good())
-        return;
+  std::string line;
+  std::getline(file, line);
+  if (!file.good()) return;
 
-    std::stringstream iss(line);
+  std::stringstream iss(line);
 
-    for (int j = 0; j < col; ++j)
-    {
-        std::string val;
-        std::getline(iss, val, ',');
-        if (!iss.good())
-            break;
+  for (int j = 0; j < col; ++j) {
+    std::string val;
+    std::getline(iss, val, ',');
+    if (!iss.good()) break;
 
-        std::stringstream convertor(val);
-        convertor >> data[j];
-    }
+    std::stringstream convertor(val);
+    convertor >> data[j];
+  }
 }
 
 // find maximum value and index
-void find_max_value_index(float *p, float *q, float d, const int K, float &delta_val, int &psi_idx)
-{
-    float tmp = 0.0;
-    int max_index = 0;
-    float max_val = p[0] * q[0] * d;
+void find_max_value_index(float *p, float *q, float d, const int K,
+                          float &delta_val, int &psi_idx) {
+  float tmp = 0.0;
+  int max_index = 0;
 
-    for (int i = 1; i != K; ++i )
-    {
-        tmp = p[i] * q[i] * d;
-        if ( max_val < tmp)
-        {
-            max_val = tmp;
-            max_index = i;
-        }
+  // p[0]: last state is 0
+  // q[0]: last state is 0, this states is i
+  // d: the emission probability of state i observe obs_seg[j]
+  float max_val = p[0] * q[0] * d;
 
+  for (int i = 1; i != K; ++i) {
+    tmp = p[i] * q[i] * d;
+    if (max_val < tmp) {
+      max_val = tmp;
+      max_index = i;
     }
+  }
 
-    delta_val = max_val;
-    psi_idx = max_index;
+  delta_val = max_val;
+  psi_idx = max_index;
 }
 
-int find_max_index(float *p, const int K)
-{
-    float max_val = p[0];
-    int max_index = 0;
+int find_max_index(float *p, const int K) {
+  float max_val = p[0];
+  int max_index = 0;
 
-    for (int i = 1; i != K; ++i )
-    {
-        if ( max_val < p[i])
-        {
-            max_val = p[i];
-            max_index = i;
-        }
+  for (int i = 1; i != K; ++i) {
+    if (max_val < p[i]) {
+      max_val = p[i];
+      max_index = i;
     }
+  }
 
-    return max_index;
+  return max_index;
 }
--- util.h
+++ util.h
@@ -22,6 +22,20 @@ void read_file_into_2d_array(const string file_name, const int row, const int co
 void read_file_into_1d_array(const string file_name, int col, float* data);
 void read_file_into_1d_array(const string file_name, int col, int* data);
 
+
+/**
+ * @brief calculate the probility of maximum path to state i at time T
+ * @param[in] delta[j-1]        the local probability of last state at time j
+ * @param[in] trans_prob[i]     the transition probability of each state to
+ * state i
+ * @param[in] emiss_prob[j][i]  the emission probaility of state i observe j
+ * @param[in] K                 the hidden states number
+ * @param[out] delta[j][i]      the local probability of state i at time j
+ * @param[out] psi[j][i]        the argmax of large probability from last states
+ * i at time j
+ * @return void
+ */
 void find_max_value_index(float *p, float *q, float d, const int K, float &delta_val, int &psi_idx);
+
 int  find_max_index(float *p, const int K);
 #endif // UTIL_H
